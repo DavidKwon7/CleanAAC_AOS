f9a132e373bcab1415e0e552d63097533420c84a
DavidKwon7 <khjunejune10@gmail.com>
2022-04-02 14:53:34 +0900

    @HiltAndroidApp & mapper

---
+++ common/src/main/java/com/example/common/mapper/Mapper.kt
@@ -0,0 +1,19 @@
+package com.example.common.mapper
+
+/**
+ *  mapper
+ */
+
+interface Mapper<I, O> {
+
+    fun from(i:I?): O
+
+    fun to(o: O?): I
+
+    fun fromList(list: List<I>?): List<O> =
+        list?.mapNotNull { from(it) } ?: listOf()
+
+    fun toList(list: List<O>?): List<I> =
+        list?.mapNotNull { to(it) } ?: listOf()
+}
+
---
+++ presentaion/src/main/java/com/example/cleanaac_aos/MyApp.kt
@@ -0,0 +1,8 @@
+package com.example.cleanaac_aos
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+}
