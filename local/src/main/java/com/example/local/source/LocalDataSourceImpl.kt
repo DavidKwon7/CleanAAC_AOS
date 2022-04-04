618eb64af159a8a8da986810b8c49b28fa49ae87
DavidKwon7 <khjunejune10@gmail.com>
2022-04-04 23:00:18 +0900

    finish sourceImpl

--- local/src/main/java/com/example/local/source/LocalDataSourceImpl.kt
+++ local/src/main/java/com/example/local/source/LocalDataSourceImpl.kt
@@ -1,21 +1,33 @@
 package com.example.local.source
 
+import com.example.common.mapper.Mapper
 import com.example.local.database.RoomDao
+import com.example.local.model.UserLocalModel
 import com.example.presentation.model.UserDataModel
 import com.example.presentation.source.LocalDataSource
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
 
-class LocalDataSourceImpl(val dao: RoomDao) : LocalDataSource {
+class LocalDataSourceImpl @Inject constructor(
+    private val dao: RoomDao,
+    private val mapper: Mapper<UserLocalModel, UserDataModel>
+) : LocalDataSource {
     override fun getLcoalUser(): Single<List<UserDataModel>> {
-        TODO("Not yet implemented")
+        return dao.getUser().map {
+            it.map {
+                mapper.from(it)
+            }
+        }
     }
 
+    // mapper의 방향이 이게 맞남? (from / to )
+
     override fun insertUser(userDataModel: UserDataModel): Completable {
-        TODO("Not yet implemented")
+        return dao.insertUser(mapper.to(userDataModel))
     }
 
     override fun deleteUser(userDataModel: UserDataModel): Completable {
-        TODO("Not yet implemented")
+        return dao.deleteUser(mapper.to(userDataModel))
     }
 }
